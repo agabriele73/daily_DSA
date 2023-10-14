006eeaa47815591359061120f16cfaf8c3d725e5
Alfonso Gabriel <agabrielejr@gmail.com>
2023-10-14 14:53:25 -0700

    Time: 42 ms (95.17%) | Memory: 41.6 MB (84.79%) - LeetSync

--- 66-plus-one/plus-one.js
+++ 66-plus-one/plus-one.js
@@ -15,17 +15,18 @@ var plusOne = function(digits) {
     // return result
 
     const n = digits.length;
-
+    // Traverse the digits array from right to left
     for (let i = n - 1; i>= 0; i--) {
         digits[i]++ 
-
+    // If the current digit becomes 10, set it to 0 and continue to the next digit
         if (digits[i] === 10) {
             digits[i] = 0;
         } else {
+             // If the current digit is not 10, there's no carry, so we can stop here.
             return digits
         }
     }
-
+    // If we reach this point, it means the entire number was 9s, and we need to add a new digit (1) at the beginning
     digits.unshift(1)
 
     return digits
